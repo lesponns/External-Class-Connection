8bbe1c86185c20712de498bf63f9ef97d0eafc41
lesponns <adamadakadam1903@gmail.com>
2023-08-15 20:26:32 +0300

    Add project files.

---
+++ SmartPro_32[15-08-23]/Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_32_15_08_23_
+{
+    internal class Method
+    {
+        public int ToplamaIslemi(int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (int i in sayilar) 
+            {
+                toplam += i;
+            }
+            return toplam;
+        }
+        public void ToplamiveOrtalamayiYazdir(int tplm, int adet)
+        {
+            int avarage;
+            avarage = tplm / adet;
+            Console.Write("\n Sayıların Toplamı: {0}\nSayıların Ortalaması: ",tplm,avarage);
+        }
+    }
+}
---
+++ SmartPro_32[15-08-23]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_32_15_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int adet, toplam;
+
+            Console.Write("Sayı adedi: ");
+            adet = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[adet];
+
+            for (int i = 0; i < adet; i++) 
+            {
+                Console.Clear();
+                Console.Write("\n{0}. Sayı: ", (i + 1));
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("\n");
+            Method hesaplama = new Method();
+
+            toplam = hesaplama.ToplamaIslemi(numbers);
+
+            hesaplama.ToplamiveOrtalamayiYazdir(toplam,numbers.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
